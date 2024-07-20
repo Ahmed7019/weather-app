d0eb4df57deb5401e61185924d66bdd445dd29a6
Ahmed7019 <Ahmedabdallah7109@gmail.com>
2024-07-20 09:29:11 +0300

    padding & margins

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Form from "./components/Form";
 function App() {
   return (
     <>
-      <div className="grid place-items-center justify-center h-[100vh] mx-8">
-        <div className="bg-blue-700 text-neutral-200 p-4 rounded-xl">
+      <div className="grid place-items-center justify-center h-[100vh] mx-12">
+        <div className="bg-blue-700 text-neutral-200 p-3 rounded-xl">
           <Form />
         </div>
       </div>
